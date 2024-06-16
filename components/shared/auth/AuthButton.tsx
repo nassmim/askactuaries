6ce61c862cb3cf5ef4cbffabf1fc965371414621
nassmim <nassmim972@gmail.com>
2024-06-16 14:21:20 +0100

    implements left sidebar navigation and right sidebar featured information

---
+++ components/shared/auth/AuthButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@components/ui/button";
+import { RouteType } from "@constants";
+import Link from "next/link";
+import React from "react";
+
+const AuthButton = ({
+  children,
+  route,
+  label,
+  className,
+}: {
+  children?: React.ReactNode;
+  route: RouteType;
+  label?: string;
+  className: string;
+}) => {
+  return (
+    <Link href={`/${route}`}>
+      <Button className={className}>
+        {label}
+        {children}
+      </Button>
+    </Link>
+  );
+};
+
+export default AuthButton;
